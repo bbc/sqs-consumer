0e0d391befeff24d5fa0edd10de1ca8b98204bc1
brando <brandoncraft@stockx.com>
2019-05-01 15:33:09 -0400

    Add dist files to github repo

---
+++ dist/consumer.d.ts
@@ -0,0 +1,52 @@
+import * as SQS from 'aws-sdk/clients/sqs';
+import { EventEmitter } from 'events';
+declare type SQSMessage = SQS.Types.Message;
+export interface ConsumerOptions {
+    queueUrl?: string;
+    attributeNames?: string[];
+    messageAttributeNames?: string[];
+    stopped?: boolean;
+    batchSize?: number;
+    visibilityTimeout?: number;
+    waitTimeSeconds?: number;
+    authenticationErrorTimeout?: number;
+    terminateVisibilityTimeout?: boolean;
+    sqs?: SQS;
+    region?: string;
+    handleMessageTimeout?: number;
+    handleMessage?(message: SQSMessage): Promise<void>;
+    handleMessageBatch?(messages: SQSMessage[]): Promise<void>;
+}
+export declare class Consumer extends EventEmitter {
+    private queueUrl;
+    private handleMessage;
+    private handleMessageBatch;
+    private handleMessageTimeout;
+    private attributeNames;
+    private messageAttributeNames;
+    private stopped;
+    private batchSize;
+    private visibilityTimeout;
+    private waitTimeSeconds;
+    private authenticationErrorTimeout;
+    private terminateVisibilityTimeout;
+    private sqs;
+    constructor(options: ConsumerOptions);
+    readonly isRunning: boolean;
+    static create(options: ConsumerOptions): Consumer;
+    start(): void;
+    stop(): void;
+    private handleSqsResponse;
+    private processMessage;
+    private receiveMessage;
+    private deleteMessage;
+    private executeHandler;
+    private terminateVisabilityTimeout;
+    private emitError;
+    private poll;
+    private processMessageBatch;
+    private deleteMessageBatch;
+    private executeBatchHandler;
+    private terminateVisabilityTimeoutBatch;
+}
+export {};
---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var consumer_1 = require("./consumer");
+exports.Consumer = consumer_1.Consumer;
