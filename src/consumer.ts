38523b5f52e5e72dc7ff4182964dcd24cc925d7e
harshpatel30 <36275162+harshpatel30@users.noreply.github.com>
2020-01-31 14:19:26 +0530

    Update consumer.ts
    fix: build

--- src/consumer.ts
+++ src/consumer.ts
@@ -304,7 +304,7 @@ export class Consumer extends EventEmitter {
         this.emit('message_processed', message);
       });
     } catch (err) {
-      this.emit('processing_error', err, message);
+      this.emit('processing_error', err, messages);
 
       if (this.terminateVisibilityTimeout) {
         try {
