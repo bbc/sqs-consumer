75367de67ef250daafaba00dee20e41fcc090b40
Andrew Schmadel <schmod@users.noreply.github.com>
2021-09-22 14:22:25 -0400

    Ensure that errors thrown in changeVisabilityTimeout are caught
    Adds a missing `await` to ensure that the error-handling code in `changeVisibilityTimeout` actually works.

--- src/consumer.ts
+++ src/consumer.ts
@@ -275,7 +275,7 @@ export class Consumer extends EventEmitter {
 
   private async changeVisabilityTimeout(message: SQSMessage, timeout: number): Promise<PromiseResult<any, AWSError>> {
     try {
-      return this.sqs
+      return await this.sqs
         .changeMessageVisibility({
           QueueUrl: this.queueUrl,
           ReceiptHandle: message.ReceiptHandle,
