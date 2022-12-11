8d513285118de89542465e6f0c41e616121a2843
Nicholas Griffin <me@nicholasgriffin.co.uk>
2022-12-11 19:48:34 +0000

    chore: updating the v3 versions

--- examples/express-simple/src/utils/sqs.ts
+++ examples/express-simple/src/utils/sqs.ts
@@ -1,34 +1,40 @@
-const { Endpoint, SQS } = require('aws-sdk');
+const { SQSClient } = require('@aws-sdk/client-sqs');
 
 const sqsConfig: {
   endpoint?: any;
-  accessKeyId?: string;
-  secretAccessKey?: string;
   region: string;
+  credentials?: {
+    accessKeyId?: string;
+    secretAccessKey?: string;
+  }
 } = {
   region: 'eu-west-1'
 };
 
 if (process.env.SQS_ENDPOINT) {
-  sqsConfig.endpoint = new Endpoint(process.env.SQS_ENDPOINT);
+  sqsConfig.endpoint = process.env.SQS_ENDPOINT;
 } else if (process.env.NODE_ENV === 'development') {
-  sqsConfig.endpoint = new Endpoint('http://localhost:4566');
+  sqsConfig.endpoint = 'http://localhost:4566';
+}
+
+if (process.env.SQS_ACCESS_KEY_ID || process.env.NODE_ENV === 'development') {
+  sqsConfig.credentials = {}
 }
 if (process.env.SQS_ACCESS_KEY_ID) {
-  sqsConfig.accessKeyId = process.env.SQS_ACCESS_KEY_ID;
+  sqsConfig.credentials.accessKeyId = process.env.SQS_ACCESS_KEY_ID;
 } else if (process.env.NODE_ENV === 'development') {
-  sqsConfig.accessKeyId = 'na';
+  sqsConfig.credentials.accessKeyId = 'na';
 }
 if (process.env.SQS_SECRET_ACCESS_KEY) {
-  sqsConfig.secretAccessKey = process.env.SQS_SECRET_ACCESS_KEY;
+  sqsConfig.credentials.secretAccessKey = process.env.SQS_SECRET_ACCESS_KEY;
 } else if (process.env.NODE_ENV === 'development') {
-  sqsConfig.secretAccessKey = 'na';
+  sqsConfig.credentials.secretAccessKey = 'na';
 }
 
 exports.sqs =
   !sqsConfig.endpoint && process.env.NODE_ENV === 'development'
     ? null
-    : new SQS(sqsConfig);
+    : new SQSClient(sqsConfig);
 
 exports.QUEUE_NAME = process.env.SQS_QUEUE_NAME || 'sqs-consumer-data';
 
