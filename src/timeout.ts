961e842c0f9091a944ee4dc4d18f0e241940ec26
Nicholas Griffin <nicholas.griffin@bbc.co.uk>
2022-12-20 22:58:55 +0000

    chore: cleaning up code (#342)
    - Moving all types to the types file
    - Moving error functions to the errors file
    - Moving validation functions to a validation file
    - Moving timeout function to a timeout file

---
+++ src/timeout.ts
@@ -0,0 +1,14 @@
+import { TimeoutResponse } from './types';
+import { TimeoutError } from './errors';
+
+function createTimeout(duration: number): TimeoutResponse[] {
+  let timeout;
+  const pending = new Promise((_, reject) => {
+    timeout = setTimeout((): void => {
+      reject(new TimeoutError());
+    }, duration);
+  });
+  return [timeout, pending];
+}
+
+export { createTimeout };
