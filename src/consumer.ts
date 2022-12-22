3ad1ca565c8e8aebfa8c68b47b400c1f8dc50ea5
Nicholas Griffin <me@nicholasgriffin.co.uk>
2022-12-22 01:25:11 +0000

    chore: restructuring consumer for a better order

--- src/consumer.ts
+++ src/consumer.ts
@@ -117,13 +117,6 @@ export class Consumer extends EventEmitter {
   }
 
   /**
-   * Returns the current polling state of the consumer: `true` if it is actively polling, `false` if it is not.
-   */
-  public get isRunning(): boolean {
-    return !this.stopped;
-  }
-
-  /**
    * Creates a new SQS consumer.
    */
   public static create(options: ConsumerOptions): Consumer {
@@ -159,6 +152,83 @@ export class Consumer extends EventEmitter {
     }
 
     this.emit('stopped');
+  }
+
+  /**
+   * Emit one of the consumer's error events depending on the error received.
+   * @param err The error object to forward on
+   * @param message The message that the error occurred on
+   */
+  private emitError(err: Error, message: Message): void {
+    if (err.name === SQSError.name) {
+      this.emit('error', err, message);
+    } else if (err instanceof TimeoutError) {
+      this.emit('timeout_error', err, message);
+    } else {
+      this.emit('processing_error', err, message);
+    }
+  }
+
+  /**
+   * Returns the current polling state of the consumer: `true` if it is actively polling, `false` if it is not.
+   */
+  public get isRunning(): boolean {
+    return !this.stopped;
+  }
+
+  /**
+   * Poll for new messages from SQS
+   */
+  private poll(): void {
+    if (this.stopped) {
+      debug('Poll was called while consumer was stopped, cancelling poll...');
+      return;
+    }
+
+    debug('Polling for messages');
+    const receiveParams: ReceiveMessageCommandInput = {
+      QueueUrl: this.queueUrl,
+      AttributeNames: this.attributeNames,
+      MessageAttributeNames: this.messageAttributeNames,
+      MaxNumberOfMessages: this.batchSize,
+      WaitTimeSeconds: this.waitTimeSeconds,
+      VisibilityTimeout: this.visibilityTimeout
+    };
+
+    let currentPollingTimeout = this.pollingWaitTimeMs;
+    this.receiveMessage(receiveParams)
+      .then(this.handleSqsResponse)
+      .catch((err) => {
+        this.emit('error', err);
+        if (isConnectionError(err)) {
+          debug('There was an authentication error. Pausing before retrying.');
+          currentPollingTimeout = this.authenticationErrorTimeout;
+        }
+        return;
+      })
+      .then(() => {
+        if (this.pollingTimeoutId) {
+          clearTimeout(this.pollingTimeoutId);
+        }
+        this.pollingTimeoutId = setTimeout(this.poll, currentPollingTimeout);
+      })
+      .catch((err) => {
+        this.emit('error', err);
+      });
+  }
+
+  /**
+   * Send a request to SQS to retrieve messages
+   * @param params The required params to receive messages from SQS
+   */
+  private async receiveMessage(
+    params: ReceiveMessageCommandInput
+  ): Promise<ReceiveMessageCommandOutput> {
+    try {
+      return await this.sqs.send(new ReceiveMessageCommand(params));
+    } catch (err) {
+      throw toSQSError(err, `SQS receive message failed: ${err.message}`);
+    }
   }
 
   /**
@@ -221,41 +291,106 @@ export class Consumer extends EventEmitter {
   }
 
   /**
-   * Send a request to SQS to retrieve messages
-   * @param params The required params to receive messages from SQS
+   * Process a batch of messages from the SQS queue.
+   * @param messages The messages that were delivered from SQS
    */
-  private async receiveMessage(
-    params: ReceiveMessageCommandInput
-  ): Promise<ReceiveMessageCommandOutput> {
+  private async processMessageBatch(messages: Message[]): Promise<void> {
+    messages.forEach((message) => {
+      this.emit('message_received', message);
+    });
+
+    let heartbeat;
     try {
-      return await this.sqs.send(new ReceiveMessageCommand(params));
+      if (this.heartbeatInterval) {
+        heartbeat = this.startHeartbeat(async () => {
+          return this.changeVisibilityTimeoutBatch(
+            messages,
+            this.visibilityTimeout
+          );
+        });
+      }
+      const ackedMessages = await this.executeBatchHandler(messages);
+
+      if (ackedMessages.length > 0) {
+        await this.deleteMessageBatch(ackedMessages);
+      }
+
+      ackedMessages.forEach((message) => {
+        this.emit('message_processed', message);
+      });
     } catch (err) {
-      throw toSQSError(err, `SQS receive message failed: ${err.message}`);
+      this.emit('error', err, messages);
+
+      if (this.terminateVisibilityTimeout) {
+        await this.changeVisibilityTimeoutBatch(messages, 0);
+      }
+    } finally {
+      clearInterval(heartbeat);
     }
   }
 
   /**
-   * Delete a single message from SQS
-   * @param message The message to delete from the SQS queue
+   * Trigger a function on a set interval
+   * @param heartbeatFn The function that should be triggered
    */
-  private async deleteMessage(message: Message): Promise<void> {
-    if (!this.shouldDeleteMessages) {
-      debug(
-        'Skipping message delete since shouldDeleteMessages is set to false'
-      );
-      return;
-    }
-    debug('Deleting message %s', message.MessageId);
+  private startHeartbeat(heartbeatFn: () => void): NodeJS.Timeout {
+    return setInterval(() => {
+      heartbeatFn();
+    }, this.heartbeatInterval * 1000);
+  }
 
-    const deleteParams: DeleteMessageCommandInput = {
-      QueueUrl: this.queueUrl,
-      ReceiptHandle: message.ReceiptHandle
-    };
-
+  /**
+   * Change the visibility timeout on a message
+   * @param message The message to change the value of
+   * @param timeout The new timeout that should be set
+   */
+  private async changeVisibilityTimeout(
+    message: Message,
+    timeout: number
+  ): Promise<ChangeMessageVisibilityCommandOutput> {
     try {
-      await this.sqs.send(new DeleteMessageCommand(deleteParams));
+      const input: ChangeMessageVisibilityCommandInput = {
+        QueueUrl: this.queueUrl,
+        ReceiptHandle: message.ReceiptHandle,
+        VisibilityTimeout: timeout
+      };
+      return await this.sqs.send(new ChangeMessageVisibilityCommand(input));
     } catch (err) {
-      throw toSQSError(err, `SQS delete message failed: ${err.message}`);
+      this.emit(
+        'error',
+        toSQSError(err, `Error changing visibility timeout: ${err.message}`),
+        message
+      );
+    }
+  }
+
+  /**
+   * Change the visibility timeout on a batch of messages
+   * @param messages The messages to change the value of
+   * @param timeout The new timeout that should be set
+   */
+  private async changeVisibilityTimeoutBatch(
+    messages: Message[],
+    timeout: number
+  ): Promise<ChangeMessageVisibilityBatchCommandOutput> {
+    const params: ChangeMessageVisibilityBatchCommandInput = {
+      QueueUrl: this.queueUrl,
+      Entries: messages.map((message) => ({
+        Id: message.MessageId,
+        ReceiptHandle: message.ReceiptHandle,
+        VisibilityTimeout: timeout
+      }))
+    };
+    try {
+      return await this.sqs.send(
+        new ChangeMessageVisibilityBatchCommand(params)
+      );
+    } catch (err) {
+      this.emit(
+        'error',
+        toSQSError(err, `Error changing visibility timeout: ${err.message}`),
+        messages
+      );
     }
   }
 
@@ -296,128 +431,52 @@ export class Consumer extends EventEmitter {
   }
 
   /**
-   * Change the visibility timeout on a message
-   * @param message The message to change the value of
-   * @param timeout The new timeout that should be set
+   * Execute the application's message batch handler
+   * @param messages The messages that should be forwarded from the SQS queue
    */
-  private async changeVisibilityTimeout(
-    message: Message,
-    timeout: number
-  ): Promise<ChangeMessageVisibilityCommandOutput> {
+  private async executeBatchHandler(messages: Message[]): Promise<Message[]> {
     try {
-      const input: ChangeMessageVisibilityCommandInput = {
-        QueueUrl: this.queueUrl,
-        ReceiptHandle: message.ReceiptHandle,
-        VisibilityTimeout: timeout
-      };
-      return await this.sqs.send(new ChangeMessageVisibilityCommand(input));
+      const result = await this.handleMessageBatch(messages);
+
+      if (result instanceof Object) {
+        return result;
+      }
+
+      return messages;
     } catch (err) {
-      this.emit(
-        'error',
-        toSQSError(err, `Error changing visibility timeout: ${err.message}`),
-        message
+      err.message = `Unexpected message handler failure: ${err.message}`;
+      throw err;
+    }
+  }
+
+  /**
+   * Delete a single message from SQS
+   * @param message The message to delete from the SQS queue
+   */
+  private async deleteMessage(message: Message): Promise<void> {
+    if (!this.shouldDeleteMessages) {
+      debug(
+        'Skipping message delete since shouldDeleteMessages is set to false'
       );
-    }
-  }
-
-  /**
-   * Emit one of the consumer's error events depending on the error received.
-   * @param err The error object to forward on
-   * @param message The message that the error occurred on
-   */
-  private emitError(err: Error, message: Message): void {
-    if (err.name === SQSError.name) {
-      this.emit('error', err, message);
-    } else if (err instanceof TimeoutError) {
-      this.emit('timeout_error', err, message);
-    } else {
-      this.emit('processing_error', err, message);
-    }
-  }
-
-  /**
-   * Poll for new messages from SQS
-   */
-  private poll(): void {
-    if (this.stopped) {
-      debug('Poll was called while consumer was stopped, cancelling poll...');
       return;
     }
+    debug('Deleting message %s', message.MessageId);
 
-    debug('Polling for messages');
-    const receiveParams: ReceiveMessageCommandInput = {
+    const deleteParams: DeleteMessageCommandInput = {
       QueueUrl: this.queueUrl,
-      AttributeNames: this.attributeNames,
-      MessageAttributeNames: this.messageAttributeNames,
-      MaxNumberOfMessages: this.batchSize,
-      WaitTimeSeconds: this.waitTimeSeconds,
-      VisibilityTimeout: this.visibilityTimeout
+      ReceiptHandle: message.ReceiptHandle
     };
 
-    let currentPollingTimeout = this.pollingWaitTimeMs;
-    this.receiveMessage(receiveParams)
-      .then(this.handleSqsResponse)
-      .catch((err) => {
-        this.emit('error', err);
-        if (isConnectionError(err)) {
-          debug('There was an authentication error. Pausing before retrying.');
-          currentPollingTimeout = this.authenticationErrorTimeout;
-        }
-        return;
-      })
-      .then(() => {
-        if (this.pollingTimeoutId) {
-          clearTimeout(this.pollingTimeoutId);
-        }
-        this.pollingTimeoutId = setTimeout(this.poll, currentPollingTimeout);
-      })
-      .catch((err) => {
-        this.emit('error', err);
-      });
-  }
-
-  /**
-   * Process a batch of messages from the SQS queue.
-   * @param message The messages that were delivered from SQS
-   */
-  private async processMessageBatch(messages: Message[]): Promise<void> {
-    messages.forEach((message) => {
-      this.emit('message_received', message);
-    });
-
-    let heartbeat;
     try {
-      if (this.heartbeatInterval) {
-        heartbeat = this.startHeartbeat(async () => {
-          return this.changeVisibilityTimeoutBatch(
-            messages,
-            this.visibilityTimeout
-          );
-        });
-      }
-      const ackedMessages = await this.executeBatchHandler(messages);
-
-      if (ackedMessages.length > 0) {
-        await this.deleteMessageBatch(ackedMessages);
-      }
-
-      ackedMessages.forEach((message) => {
-        this.emit('message_processed', message);
-      });
+      await this.sqs.send(new DeleteMessageCommand(deleteParams));
     } catch (err) {
-      this.emit('error', err, messages);
-
-      if (this.terminateVisibilityTimeout) {
-        await this.changeVisibilityTimeoutBatch(messages, 0);
-      }
-    } finally {
-      clearInterval(heartbeat);
+      throw toSQSError(err, `SQS delete message failed: ${err.message}`);
     }
   }
 
   /**
    * Delete a batch of messages from the SQS queue.
-   * @param message The messages that should be deleted from SQS
+   * @param messages The messages that should be deleted from SQS
    */
   private async deleteMessageBatch(messages: Message[]): Promise<void> {
     if (!this.shouldDeleteMessages) {
@@ -444,64 +503,5 @@ export class Consumer extends EventEmitter {
     } catch (err) {
       throw toSQSError(err, `SQS delete message failed: ${err.message}`);
     }
-  }
-
-  /**
-   * Execute the application's message batch handler
-   * @param message The messages that should be forwarded from the SQS queue
-   */
-  private async executeBatchHandler(messages: Message[]): Promise<Message[]> {
-    try {
-      const result = await this.handleMessageBatch(messages);
-
-      if (result instanceof Object) {
-        return result;
-      }
-
-      return messages;
-    } catch (err) {
-      err.message = `Unexpected message handler failure: ${err.message}`;
-      throw err;
-    }
-  }
-
-  /**
-   * Change the visibility timeout on a batch of messages
-   * @param message The messages to change the value of
-   * @param timeout The new timeout that should be set
-   */
-  private async changeVisibilityTimeoutBatch(
-    messages: Message[],
-    timeout: number
-  ): Promise<ChangeMessageVisibilityBatchCommandOutput> {
-    const params: ChangeMessageVisibilityBatchCommandInput = {
-      QueueUrl: this.queueUrl,
-      Entries: messages.map((message) => ({
-        Id: message.MessageId,
-        ReceiptHandle: message.ReceiptHandle,
-        VisibilityTimeout: timeout
-      }))
-    };
-    try {
-      return await this.sqs.send(
-        new ChangeMessageVisibilityBatchCommand(params)
-      );
-    } catch (err) {
-      this.emit(
-        'error',
-        toSQSError(err, `Error changing visibility timeout: ${err.message}`),
-        messages
-      );
-    }
-  }
-
-  /**
-   * Trigger a function on a set interval
-   * @param heartbeatFn The function that should be triggered
-   */
-  private startHeartbeat(heartbeatFn: () => void): NodeJS.Timeout {
-    return setInterval(() => {
-      heartbeatFn();
-    }, this.heartbeatInterval * 1000);
   }
 }
