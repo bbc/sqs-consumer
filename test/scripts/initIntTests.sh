9735d06cbbb9569e20bd95eec0daf361a8fe4e02
Nicholas Griffin <me@nicholasgriffin.co.uk>
2023-01-22 20:10:49 +0000

    chore: rewording

--- test/scripts/initIntTests.sh
+++ test/scripts/initIntTests.sh
@@ -12,11 +12,11 @@ export AWS_ACCESS_KEY_ID="key"
 export AWS_SECRET_ACCESS_KEY="secret"
 export AWS_DEFAULT_REGION="eu-west-1"
 
-echo "Waiting for SQS at address $QUEUE_URL, attempting every 5s"
+echo "Waiting for SQS, attempting every 5s"
 until $(aws --endpoint-url=http://localhost:4566 sqs get-queue-url --queue-name sqs-consumer-data --region eu-west-1 | grep "{
     "QueueUrl": "http://localhost:4566/000000000000/sqs-consumer-data"
 }" > /dev/null); do
     printf '.'
     sleep 5
 done
-echo ' Success: Reached SQS'
+echo ' Service is up!'
