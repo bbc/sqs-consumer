a2e7739f8994fa81f325049e88040ac4017bdb7e
Nicholas Griffin <me@nicholasgriffin.co.uk>
2023-01-22 20:07:03 +0000

    chore: try waiting for SQS queue instead

--- test/scripts/initIntTests.sh
+++ test/scripts/initIntTests.sh
@@ -8,9 +8,15 @@ else
   exit 1
 fi
 
-while curl -s http://localhost:4566/health | grep -v "\"initScripts\": \"initialized\""; do
-    printf 'SQS is unavailable - sleeping'
-    sleep 10s
-done
+export AWS_ACCESS_KEY_ID="key"
+export AWS_SECRET_ACCESS_KEY="secret"
+export AWS_DEFAULT_REGION="eu-west-1"
 
-echo "Service is up and running"
+echo "Waiting for SQS at address $QUEUE_URL, attempting every 5s"
+until $(aws --endpoint-url=http://localhost:4566 sqs get-queue-url --queue-name sqs-consumer-data --region eu-west-1 | grep "{
+    "QueueUrl": "http://localhost:4566/000000000000/sqs-consumer-data"
+}" > /dev/null); do
+    printf '.'
+    sleep 5
+done
+echo ' Success: Reached SQS'
