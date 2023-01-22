5678984f342750f8c9c107f0d5831151c3ea5bd4
Nicholas Griffin <me@nicholasgriffin.co.uk>
2023-01-22 19:58:23 +0000

    chore: try waiting for health check

--- test/scripts/initIntTests.sh
+++ test/scripts/initIntTests.sh
@@ -7,3 +7,10 @@ else
   echo "Could not start docker" >&2
   exit 1
 fi
+
+while curl -s http://localhost:4566/health | grep -v "\"initScripts\": \"initialized\""; do
+    printf 'SQS is unavailable - sleeping'
+    sleep 10s
+done
+
+echo "Service is up and running"
