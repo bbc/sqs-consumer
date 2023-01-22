eb092473fbf044f93ddd34ab020fbd7c752e5954
Nicholas Griffin <me@nicholasgriffin.co.uk>
2023-01-22 22:56:02 +0000

    chore: increasing the polling wait time

--- test/features/utils/consumer/handleMessage.js
+++ test/features/utils/consumer/handleMessage.js
@@ -5,6 +5,7 @@ const { QUEUE_URL, sqs } = require('../sqs');
 const consumer = Consumer.create({
   queueUrl: QUEUE_URL,
   sqs,
+  pollingWaitTimeMs: 100,
   handleMessage: async (message) => {
     return message;
   }
--- test/features/utils/consumer/handleMessageBatch.js
+++ test/features/utils/consumer/handleMessageBatch.js
@@ -5,6 +5,7 @@ const { QUEUE_URL, sqs } = require('../sqs');
 const consumer = Consumer.create({
   queueUrl: QUEUE_URL,
   sqs,
+  pollingWaitTimeMs: 100,
   batchSize: 5,
   handleMessageBatch: async (messages) => {
     return messages;
