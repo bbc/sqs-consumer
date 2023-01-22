cbeb3ae23cbfa76f3191c5163b39cc2e2690fd02
Nicholas Griffin <me@nicholasgriffin.co.uk>
2023-01-22 19:25:10 +0000

    chore: adding tests for multiple messages

--- test/features/handleMessage.feature
+++ test/features/handleMessage.feature
@@ -2,6 +2,8 @@ Feature: When handleMessage is used, messages are consumed without error
 
   Scenario: A message is consumed from SQS
     Given a message is sent to the SQS queue
-    When SQS Consumer is started
     Then the message should be consumed without error
-    Then SQS Consumer should stop
+
+  Scenario: Multiple messages are consumed from SQS
+    Given messages are sent to the SQS queue
+    Then the messages should be consumed without error
--- test/features/step_definitions/handleMessage.js
+++ test/features/step_definitions/handleMessage.js
@@ -1,13 +1,11 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
+const { Given, Then } = require('@cucumber/cucumber');
 const assert = require('assert');
 const { PurgeQueueCommand } = require('@aws-sdk/client-sqs');
+const pEvent = require('p-event');
 
 const { consumer } = require('../utils/consumer/handleMessage');
 const { producer } = require('../utils/producer');
 const { sqs, QUEUE_URL } = require('../utils/sqs');
-const { delay } = require('../utils/delay');
-
-const POLLING_TIMEOUT = 100;
 
 Given('a message is sent to the SQS queue', async () => {
   const params = {
@@ -25,26 +23,63 @@ Given('a message is sent to the SQS queue', async () => {
   assert.strictEqual(size, 1);
 });
 
-When('SQS Consumer is started', () => {
+Then('the message should be consumed without error', async () => {
   consumer.start();
 
   const isRunning = consumer.isRunning;
 
   assert.strictEqual(isRunning, true);
-});
 
-Then('the message should be consumed without error', async () => {
+  await pEvent(consumer, 'message_processed');
+
   const size = await producer.queueSize();
 
-  await delay(POLLING_TIMEOUT);
-
   assert.strictEqual(size, 0);
-});
 
-Then('SQS Consumer should stop', () => {
   consumer.stop();
 
-  const isRunning = consumer.isRunning;
+  assert.strictEqual(consumer.isRunning, false);
+});
 
-  assert.strictEqual(isRunning, false);
+Given('messages are sent to the SQS queue', async () => {
+  const params = {
+    QueueUrl: QUEUE_URL
+  };
+  const command = new PurgeQueueCommand(params);
+  const response = await sqs.send(command);
+
+  assert.strictEqual(response['$metadata'].httpStatusCode, 200);
+
+  await producer.send(['msg2', 'msg3', 'msg4']);
+
+  const size = await producer.queueSize();
+
+  assert.strictEqual(size, 3);
+});
+
+Then('the messages should be consumed without error', async () => {
+  consumer.start();
+
+  assert.strictEqual(consumer.isRunning, true);
+
+  await pEvent(consumer, 'message_processed');
+  const size = await producer.queueSize();
+
+  assert.strictEqual(size, 2);
+
+  await pEvent(consumer, 'message_processed');
+
+  const size2 = await producer.queueSize();
+
+  assert.strictEqual(size2, 1);
+
+  await pEvent(consumer, 'message_processed');
+
+  const size3 = await producer.queueSize();
+
+  assert.strictEqual(size3, 0);
+
+  consumer.stop();
+
+  assert.strictEqual(consumer.isRunning, false);
 });
