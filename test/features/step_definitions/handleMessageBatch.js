ff3a7ff6ce231146e1e9dfc4aa81e5f8b1576186
Nicholas Griffin <me@nicholasgriffin.co.uk>
2023-01-27 23:56:36 +0000

    chore: minor test updates

--- test/features/step_definitions/handleMessageBatch.js
+++ test/features/step_definitions/handleMessageBatch.js
@@ -8,6 +8,7 @@ const { producer } = require('../utils/producer');
 const { sqs, QUEUE_URL } = require('../utils/sqs');
 
 let messageIdsTestOne;
+let messageIdsTestTwo;
 
 Given('a message batch is sent to the SQS queue', async () => {
   const params = {
@@ -63,7 +64,9 @@ Given('message batches are sent to the SQS queue', async () => {
 
   assert.strictEqual(response['$metadata'].httpStatusCode, 200);
 
-  await producer.send(['msg1', 'msg2', 'msg3', 'msg4', 'msg5', 'msg6']);
+  const msg = await producer.send(['msg1', 'msg2', 'msg3', 'msg4', 'msg5', 'msg6']);
+
+  messageIdsTestTwo = msg.map((m) => m.MessageId);
 
   const size = await producer.queueSize();
 
@@ -85,18 +88,18 @@ Then(
     const size = await producer.queueSize();
     assert.strictEqual(size, 1);
     assert.strictEqual(consumer.messagesInQueue.length, 4);
-    assert.strictEqual(consumer.messagesInQueue[0], messageIdsTestOne[0]);
-    assert.strictEqual(consumer.messagesInQueue[1], messageIdsTestOne[1]);
-    assert.strictEqual(consumer.messagesInQueue[2], messageIdsTestOne[2]);
-    assert.strictEqual(consumer.messagesInQueue[3], messageIdsTestOne[3]);
-    assert.strictEqual(consumer.messagesInQueue[4], messageIdsTestOne[4]);
+    assert.strictEqual(consumer.messagesInQueue[0], messageIdsTestTwo[0]);
+    assert.strictEqual(consumer.messagesInQueue[1], messageIdsTestTwo[1]);
+    assert.strictEqual(consumer.messagesInQueue[2], messageIdsTestTwo[2]);
+    assert.strictEqual(consumer.messagesInQueue[3], messageIdsTestTwo[3]);
+    assert.strictEqual(consumer.messagesInQueue[4], messageIdsTestTwo[4]);
 
     await pEvent(consumer, 'message_received');
 
     const size2 = await producer.queueSize();
     assert.strictEqual(size2, 0);
     assert.strictEqual(consumer.messagesInQueue.length, 1);
-    assert.strictEqual(consumer.messagesInQueue[5], messageIdsTestOne[0]);
+    assert.strictEqual(consumer.messagesInQueue[5], messageIdsTestTwo[5]);
 
     consumer.stop();
     assert.strictEqual(consumer.isRunning, false);
