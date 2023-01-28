7986c198bf39db0427cbca96b97b2b7aa473a962
Nicholas Griffin <me@nicholasgriffin.co.uk>
2023-01-28 14:38:48 +0000

    fix: adjusting wait time back

--- test/features/utils/consumer/handleMessage.js
+++ test/features/utils/consumer/handleMessage.js
@@ -5,7 +5,7 @@ const { QUEUE_URL, sqs } = require('../sqs');
 const consumer = Consumer.create({
   queueUrl: QUEUE_URL,
   sqs,
-  pollingWaitTimeMs: 300,
+  pollingWaitTimeMs: 100,
   handleMessage: async (message) => {
     return message;
   }
