2d5b3696f1d44463c04fa669b6d0bc0bfe5152bd
Nicholas Griffin <me@nicholasgriffin.co.uk>
2023-05-12 23:01:43 +0100

    fix: actually removing emitter from types

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,4 @@
 import { SQSClient, Message } from '@aws-sdk/client-sqs';
-import { EventEmitter } from 'events';
-
-import { logger } from './logger';
 
 export interface ConsumerOptions {
   /**
@@ -170,39 +167,6 @@ export interface Events {
    * Fired when an option is updated
    */
   option_updated: [UpdatableOptions, ConsumerOptions[UpdatableOptions]];
-}
-
-export class TypedEventEmitter extends EventEmitter {
-  /**
-   * Trigger a listener on all emitted events
-   * @param event The name of the event to listen to
-   * @param listener A function to trigger when the event is emitted
-   */
-  on<E extends keyof Events>(
-    event: E,
-    listener: (...args: Events[E]) => void
-  ): this {
-    return super.on(event, listener);
-  }
-  /**
-   * Trigger a listener only once for an emitted event
-   * @param event The name of the event to listen to
-   * @param listener A function to trigger when the event is emitted
-   */
-  once<E extends keyof Events>(
-    event: E,
-    listener: (...args: Events[E]) => void
-  ): this {
-    return super.once(event, listener);
-  }
-  /**
-   * Emits an event with the provided arguments
-   * @param event The name of the event to emit
-   */
-  emit<E extends keyof Events>(event: E, ...args: Events[E]): boolean {
-    logger.debug(event, ...args);
-    return super.emit(event, ...args);
-  }
 }
 
 export type AWSError = {
