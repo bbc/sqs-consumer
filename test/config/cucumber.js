0a58021eb53041cf02b68dcf522ab983e9a034ed
Nicholas Griffin <nicholas.griffin@bbc.co.uk>
2023-10-16 14:58:31 +0100

    chore: updating dependencies to the latest versions (#430)
    * chore: updating dependencies to the latest versions
    
    * chore: swapping iplayer eslint package
    
    * chore: also use the base eslint config

--- test/config/cucumber.js
+++ test/config/cucumber.js
@@ -2,7 +2,6 @@ module.exports = {
   default: {
     parallel: 0,
     format: ['html:test/reports/cucumber-report.html'],
-    publishQuiet: true,
     paths: ['test/features'],
     forceExit: true
   }
