b7ba4842e4f67c4235bcaca3ff1b9c4c6e573e83
Nicholas <nicholas.griffin@bbc.co.uk>
2024-05-06 16:47:02 +0100

    chore: swap out artefacts for annotions

--- test/config/cucumber.mjs
+++ test/config/cucumber.mjs
@@ -1,6 +1,6 @@
 export default {
   parallel: 0,
-  format: ['html:test/reports/cucumber-report.html'],
-  paths: ['test/features'],
-  forceExit: true
+  format: ["message:test/reports/cucumber-report.ndjson"],
+  paths: ["test/features"],
+  forceExit: true,
 };
