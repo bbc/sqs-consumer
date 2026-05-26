a2b699cf26ae1de480b086ac8f23cbb12eb16e3a
Nicholas Griffin <me@nicholasgriffin.co.uk>
2026-05-26 21:38:31 +0100

    chore: remove git step

--- release.config.cjs
+++ release.config.cjs
@@ -35,13 +35,6 @@ module.exports = {
     ],
     "@semantic-release/changelog",
     "@semantic-release/npm",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["package.json", "package-lock.json", "CHANGELOG.md"],
-        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
     "@semantic-release/github",
     "@sebbo2002/semantic-release-jsr",
   ],
